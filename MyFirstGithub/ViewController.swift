ec0707c4acdbff206dc6a1697ce267e13116e459
Ben Wong <benben3445@hotmail.com>
2016-11-16 19:41:26 +0800

    made a change
    Ha Ha

--- MyFirstGithub/ViewController.swift
+++ MyFirstGithub/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World")
+        print("Hey! how are you?")
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
